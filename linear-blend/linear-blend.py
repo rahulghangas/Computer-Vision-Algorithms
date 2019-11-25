b115cd012a4cc8e659b26cd501309e5e3210c7a4
Neco Kriel <necokriel@gmail.com>
2019-11-25 23:26:46 +1100

    Add: mirror of animation and commented.

--- linear-blend/linear-blend.py
+++ linear-blend/linear-blend.py
@@ -7,14 +7,15 @@ from matplotlib import animation
 from matplotlib import pyplot as plt
 from matplotlib.image import imread
 
+# check that python version is 3
 if sys.version_info[0] != 3:
     raise Exception("Python3 should be used with this module")
 
-
+# Rate of animations [per second]
 FRAMES_PER_SEC = 50
 
-
 def import_images():
+    # Get user input
     image1_path = input("Enter path for first image\n")
     image2_path = input("Enter path for second image\n")
 
@@ -24,27 +25,38 @@ def import_images():
 
     time = int(input("Enter duration(in seconds) for blending: "))
 
+    # Run animation
     blend(img1, img2, time)
-
 
 def blend(img1, img2, time):
     fig = plt.figure()
+
+    # Initialise animation frame (matrix)
     blended = np.zeros(img1.shape)
 
+    # Initialise list of images
     imgs = []
-    global FRAMES_PER_SEC
-    for i in range(FRAMES_PER_SEC * time):
-        alpha = i / (FRAMES_PER_SEC * time)
 
+    # Global variable
+    global FRAMES_PER_SEC
+
+    # Loop cycles over specified number of frames
+    for i in range(FRAMES_PER_SEC * time // 2):
+        # Increment the opacity variable (0 -> 1)
+        alpha = 2 * i / (FRAMES_PER_SEC * time)
+        # Generate next frame: scale images inversily (sum of opacity = 1)
         blended = np.array((1 - alpha) * img1 + alpha * img2, dtype=np.uint8)
+        # Append image to animation (list of frames)
         imgs.append([plt.imshow(blended, animated=True)])
 
-    ani = animation.ArtistAnimation(
-        fig, imgs, interval=(1 / FRAMES_PER_SEC * 1000), repeat_delay=50
-    )
+    # add mirror animation to current animations
+    imgs += imgs[::-1]
+    
+    # Run animation
+    ani = animation.ArtistAnimation(fig, imgs, interval=(1 / FRAMES_PER_SEC * 1000))
 
+    # Necessary finish
     plt.show()
-
 
 if __name__ == "__main__":
     import_images()
